520ff291eb5eafaf0e94670d5812931431e42813
Jerrian30 <99036268+Jerrian30@users.noreply.github.com>
2022-02-06 22:08:09 +0700

    Add files via upload
    NPM : G1A021039
    NAMA : JERRIAN
    
    Penjelasan
    void, merupakan fungsi yang digunakan jika fungsi tersebut tidak menghasilkan nilai keluaran dari proses yang ada didalamnya.
    cout, berfungsi untuk menampilkan data (fungsi keluaran)
    cin, (c input) adalah fungsi untuk mengambil input dari keyboard.
    Int, fungsinya untuk mengelompokan variabel yang isinya bilangan bulat.

---
+++ penerapanADT.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+void main()
+{
+ int a,b,c,d,e,total,hasil;
+ cout<<“Masukan nilai pertama: “;
+ cin>>a;
+ cout<<“Masukan nilai kedua: “;
+ cin>>b;
+ cout<<“Masukan nilai ketiga: “;
+ cin>>c;
+ cout<<“Masukan nilai keempat: “;
+ cin>>d;
+ cout<<“Masukan nilai kelima: “;
+ cin>>e;
+ total=a+b+c+d+e;
+ hasil=total/5;
+ cout<<“Jumlah rata-rata dari lima bilangan tersebut adalah: “<<hasil<<endl;
+}
